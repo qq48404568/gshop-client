53615a68a7a2ced902efaff309d36042383951f2
qq48404568 <48404568@qq.com>
2021-10-19 17:48:19 +0800

    新增了商品列表功能

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import './mock/mockServer'; //加载mockServer即可
+
 Vue.config.productionTip = false
 Vue.component(Button.name,Button) // <mt-button>
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ import Search from '../pages/Search/Search.vue';
 import Order from '../pages/Order/Order.vue';
 import Profile from '../pages/Profile/Profile.vue';
 import Login from '../pages/Login/Login.vue';
+import Shop from '../pages/Shop/Shop.vue';
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue';
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue';
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue';
+
 
 
 Vue.use(Router)
@@ -47,6 +52,28 @@ export default new Router({
     {
       path:'/login',
       component:Login
+    },
+    {
+      path:'/shop',
+      component:Shop,
+      children:[
+        {
+          path:'/shop/goods',
+          component:ShopGoods,
+        },
+        {
+          path:'/shop/ratings',
+          component:ShopRatings,
+        },
+        {
+          path:'/shop/info',
+          component:ShopInfo,
+        },
+        {
+          path:'',
+          redirect:'/shop/goods',
+        },
+      ]
     },
   ],
   mode:'history'
